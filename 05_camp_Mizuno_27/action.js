4ae8258ad86f7e40a27927b83500df4581702e5f
mizuno3to3 <satomi2zuno@gmail.com>
2019-08-26 00:32:17 +0900

    チーズアカデミーにJavaScriptをつけてみました

---
+++ 05_camp_Mizuno_27/action.js
@@ -0,0 +1,80 @@
+//Aboutについて
+$(".t1").on("mouseover", function(){
+    //   マウスを合わせたらされたら実行したいことを書く
+    $(".t1").css('color','white');
+    $(".t1").css('background-color','lightpink');
+});
+$(".t1").on("mouseout", function(){
+    //   マウスを合わせたらされたら実行したいことを書く
+    $(".t1").css('color','black');
+    $(".t1").css('background-color','white');
+});
+
+//COURSEについて
+$(".t2").on("mouseover", function(){
+    //   マウスを合わせたらされたら実行したいことを書く
+    $(".t2").css('color','white');
+    $(".t2").css('background-color','lightpink');
+});
+$(".t2").on("mouseout", function(){
+    //   マウスを合わせたらされたら実行したいことを書く
+    $(".t2").css('color','black');
+    $(".t2").css('background-color','white');
+});
+
+
+//NEWSについて
+$(".t3").on("mouseover", function(){
+    //   マウスを合わせたらされたら実行したいことを書く
+    $(".t3").css('color','white');
+    $(".t3").css('background-color','lightpink');
+});
+$(".t3").on("mouseout", function(){
+    //   マウスを合わせたらされたら実行したいことを書く
+    $(".t3").css('color','black');
+    $(".t3").css('background-color','white');
+});
+
+//ACCESSについて
+$(".t4").on("mouseover", function(){
+    //   マウスを合わせたらされたら実行したいことを書く
+    $(".t4").css('color','white');
+    $(".t4").css('background-color','lightpink');
+});
+$(".t4").on("mouseout", function(){
+    //   マウスを合わせたらされたら実行したいことを書く
+    $(".t4").css('color','black');
+    $(".t4").css('background-color','white');
+});
+
+
+//CONTACTについて
+$(".t5").on("mouseover", function(){
+    //   マウスを合わせたらされたら実行したいことを書く
+    $(".t5").css('color','white');
+    $(".t5").css('background-color','lightpink');
+});
+$(".t5").on("mouseout", function(){
+    //   マウスを合わせたらされたら実行したいことを書く
+    $(".t5").css('color','black');
+    $(".t5").css('background-color','white');
+});
+
+
+//セカイを変えるチーズを作ろうについて
+$(".mv1").on("click", function(){
+    //   clickされたら実行したいことを書く
+    $(".mv1").css('color','red');
+    $(".mv1").fadeOut(1000);
+    $(".mv1").fadeIn(1000);
+});
+
+//チーズ職人養成学校「チーズアカデミーTOKYO」について
+$(".mv2").on("click", function(){
+    //   clickされたら実行したいことを書く
+    $(".mv2").css('color','green');
+    $(".mv2").fadeOut(1000);
+    $(".mv2").fadeIn(1000);
+});
+
+//
